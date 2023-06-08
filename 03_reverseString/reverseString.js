f2cf9d3d47eda46e66ec88dfc05a021838d31200
sreeja <sreeja.guduri@gmail.com>
2023-06-08 16:37:01 +0400

    finished 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let len = string.length;
+    let array = string.split("");
+    let ans = "";
 
+    for(let i=0;i<len;i++)
+    {
+        if(array[len-i-1]===null || array[len-i-1]===undefined)
+        {
+            continue;
+        }
+        ans += `${array[len-i-1]}`;
+    }
+    return ans;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(nums, ...moreargs) {
+    let index;
+    let len=nums.length;
+    let size = moreargs.length;
 
+    for(let i=0;i<len;i++)
+    {
+        for(let j=0;j<size;j++)
+        {
+            if(nums[i] === moreargs[j]){
+                nums.splice(i,1);
+                moreargs.splice(j,1);
+                i--;
+                len--;
+                size--;
+                j--;
+                break;
+            }
+        }
+    }
+    return nums;
 };
 
 // Do not edit below this line
